96f26ebda5d4a9f2e0d4d704393f6602691015ff
Delushaan <mdelushaan@hotmail.com>
2020-12-09 11:20:56 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SL_StockTrade.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
