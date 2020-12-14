eaab9739ce744ca8ebb2e2e84384c244b24d8825
Delushaan <mdelushaan@hotmail.com>
2020-12-14 18:20:47 +0800

    edit role completed

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -5,16 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SL_StockTrade.Models;
+using SL_StockTrade.ViewModel;
 
 namespace SL_StockTrade.Controllers
 {
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<SellerDeviceData> usermanager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(RoleManager<IdentityRole> roleManager, 
+                                UserManager<SellerDeviceData> usermanager)
         {
             this.roleManager = roleManager;
+            this.usermanager = usermanager;
         }
 
         [HttpGet]
@@ -55,6 +59,60 @@ namespace SL_StockTrade.Controllers
             var roles = roleManager.Roles;
 
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string Id)
+        {
+            var role = await roleManager.FindByIdAsync(Id);
+
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {Id} can not be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            foreach(var user in usermanager.Users)
+            {
+                if(await usermanager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {model.Id} can not be found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var res = await roleManager.UpdateAsync(role);
+
+                if(res.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+            }
+
+            return View(model);
+
         }
     }
 }
---
+++ ViewModel/EditRoleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SL_StockTrade.ViewModel
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<String>();
+        }
+
+        public string Id { get; set; }
+
+        [Required(ErrorMessage =  "Role Name is Required")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}
